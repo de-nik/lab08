6d03194cf09bb9fcd094f8773f3c8f9d2a3c11ea
de-nik <bmstu.nikulin@yandex.ru>
2017-09-17 14:12:27 +0300

    added sources

---
+++ examples/example1.cpp
@@ -0,0 +1,5 @@
+#include <print.hpp>
+
+int main(int argc, char** argv) {
+print("hello");
+}
---
+++ include/print.hpp
@@ -0,0 +1,5 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+void print(const std::string& text, std::ostream& out = std::cout);
+void print(const std::string& text, std::ofstream& out);
