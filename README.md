0fac98cf2ed56ee1969a880b1bd7e91163f6c7a5
de-nik <bmstu.nikulin@yandex.ru>
2017-10-22 17:15:22 +0300

    added doxygen.conf

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-[![Build Status](https://travis-ci.org/de-nik/lab06.svg?branch=master)](https://travis-ci.org/de-nik/lab06)
+[![Build Status](https://travis-ci.org/de-nik/lab07.svg?branch=master)](https://travis-ci.org/de-nik/lab07)
 
--- include/print.hpp
+++ include/print.hpp
@@ -1,5 +1,15 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+//!Функция вывода строки в поток вывода.
+/*!
+      \param text аргумент типа std::string.
+      \param out агрумент типа std::ostream, по умолчанию std::cout.
+    */
 void print(const std::string& text, std::ostream& out = std::cout);
+//!Функция вывода строки в файловый поток.
+/*!
+      \param text аргумент типа std::string.
+      \param out агрумент типа std::ofstream.
+    */
 void print(const std::string& text, std::ofstream& out);
